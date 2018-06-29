b7c6a4b008cc79ffd58298e124e582185ea97706
Gil Lopes Bueno <gil.bueno@martinlabs.com.br>
2018-06-29 14:49:38 -0300

    unit tests passing

---
+++ src/NeoSharp.Core/Helpers/ContractHelper.cs
@@ -0,0 +1,74 @@
+﻿using NeoSharp.Core.Models;
+using NeoSharp.VM;
+using NeoSharp.Core.Extensions;
+using NeoSharp.Core.Cryptography;
+using NeoSharp.Core.SmartContract;
+using System;
+using System.Linq;
+
+namespace NeoSharp.Core.Helpers
+{
+    public class ContractHelper
+    {
+        /// <summary>
+        /// Creates the single public key redeem contract ('regular account')
+        /// </summary>
+        /// <returns>The single public key redeem contract.</returns>
+        /// <param name="scrypt">Scrypt.</param>
+        /// <param name="publicKey">Public key.</param>
+        public Contract CreateSinglePublicKeyRedeemContract(ICrypto scrypt, ECPoint publicKey){
+            string contractHexCode;
+            using (ScriptBuilder sb = new ScriptBuilder())
+            {
+                sb.EmitPush(publicKey.EncodedData);
+                sb.Emit(EVMOpCode.CHECKSIG);
+                contractHexCode = sb.ToArray().ToHexString();
+            }
+
+            ContractParameterType returnType = ContractParameterType.Void; 
+            ContractParameterType[] parameters = { ContractParameterType.Signature };
+
+            //var contractCode = new Code(scrypt, contractHexCode, returnType, parameters);
+            //var contract = new Contract(contractCode);
+
+            return null; // contract;
+        }
+
+
+        /// <summary>
+        /// Creates the multiple public key redeem contract ('multisig account')
+        /// </summary>
+        /// <returns>The multiple public key redeem contract.</returns>
+        /// <param name="scrypt">Scrypt.</param>
+        /// <param name="numberOfRequiredPublicKeys">Number of required public keys.</param>
+        /// <param name="publicKeys">Public keys.</param>
+        public Contract CreateMultiplePublicKeyRedeemContract(ICrypto scrypt, int numberOfRequiredPublicKeys, ECPoint[] publicKeys){
+            string contractHexCode;
+
+            if (!((1 <= numberOfRequiredPublicKeys) 
+                  && (numberOfRequiredPublicKeys <= publicKeys.Length) 
+                  && (publicKeys.Length <= 1024)))
+                throw new ArgumentException("Invalid public keys. ");
+            
+            using (ScriptBuilder sb = new ScriptBuilder())
+            {
+                sb.EmitPush(numberOfRequiredPublicKeys);
+                foreach (ECPoint publicKey in publicKeys.OrderBy(p => p))
+                {
+                    sb.EmitPush(publicKey.EncodedData);
+                }
+                sb.EmitPush(publicKeys.Length);
+                sb.Emit(EVMOpCode.CHECKMULTISIG);
+                contractHexCode = sb.ToArray().ToHexString();
+            }
+
+            ContractParameterType returnType = ContractParameterType.Void; 
+            ContractParameterType[] parameters = Enumerable.Repeat(ContractParameterType.Signature, numberOfRequiredPublicKeys).ToArray();
+
+            //var contractCode = new Code(scrypt, contractHexCode, returnType, parameters);
+            //var contract = new Contract(contractCode);
+
+            return null; // contract;
+        }
+    }
+}
