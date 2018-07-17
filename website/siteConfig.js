8f54cacec3c0dfaca0e889fdc878d120dea22813
Gil Lopes Bueno <gil.bueno@martinlabs.com.br>
2018-07-17 11:42:00 +0800

    organizationname

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -31,7 +31,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'neo-sharp',
-  organizationName: 'City of Zion',
+  organizationName: 'simplitech',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
